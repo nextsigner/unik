5794ec336db30a31c99c0042955983c2546c54f6
nextsigner <nextsigner@gmail.com>
2018-06-26 16:45:19 +0000

    se agregan recursos para rpi3

---
+++ rpi_resources/remove_qt5_rpi3.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo apt-get remove qt5-default && sudo apt-get remove qtdeclarative5-* && sudo apt-get remove qml-module-qtquick*  && sudo apt-get remove qtquick1-* && sudo apt-get remove qtquickcontrols5-*  && sudo apt-get remove qml-module-qtquick2 && sudo apt-get remove qtcreator
+
