5ec4936b7b6edb9ab2ac880f738b060aef92ea6f
nextsigner <nextsigner@gmail.com>
2020-01-12 14:25:34 -0300

    se optimiza la creación de deploy.sh en linux.pri

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-echo Iniciando deploy.sh
+echo Iniciando deploy.sh...
 cd ../
 cd ./unik-dev-apps/unik
+~/linuxdeployqt-continuous-x86_64.AppImage /media/nextsigner/ZONA-A11/nsp/unik/build_linux/unik -qmldir=/media/nextsigner/ZONA-A11/nsp/unik -qmake=/home/nextsigner/Qt/5.12.3/gcc_64/bin/qmake -verbose=3 -bundle-non-qt-libs -no-plugins -appimage && cp unik_v4.02048-x86_64.AppImage ~/unik_v4.02048-x86_64.AppImage && rm -f /usr/local/bin/unik && cd ~/ && ln ~/unik_v4.02048-x86_64.AppImage /usr/local/bin/unik && unik -install
